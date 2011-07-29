b5467a2bbaddd857f09448e9ce9bef67f593f152
Maxime Haineault <max@motion-m.ca>
2011-07-29 05:02:29 +0000

    frontadmin bar button edit mode, admin, disconnect now works.

--- webcore/contrib/frontadmin/static/frontadmin/js/jquery.frontadmin.js
+++ webcore/contrib/frontadmin/static/frontadmin/js/jquery.frontadmin.js
@@ -3,19 +3,36 @@ $(function(){
     $.frontadmin = (function(){
         var $self = this
         
-        $self.initialized = false
+        $self.states = {
+            initialized: false,
+            toolbars_visibles: false
+        }
 
         $self.bar = $('#frontadmin-bar-frame')
         $self.toolbars = $('.frontadmin-toolbar-frame')
 
-        $self.button = {
+        $self.buttons = {
             logout: '#frontadmin-btn-logout',
+            toggle: '#frontadmin-btn-toggle',
         }
+
+        $self.cookie = function(k, v) {
+            if (v) { return $.cookie(k, v, {path: '/'}) }
+            else   { return $.cookie(k,    {path: '/'}) }
+        }
+
+        // frontadmin toolbar initial state
+        if ($self.cookie('frontadmin_toolbars_visibles') == null) { 
+            // Cookie does not exist, set it and show the toolbar by default
+            $self.cookie('frontadmin_toolbars_visibles', 'true')
+            $self.states.toolbars_visibles = true
+        }
+        $('html')[($self.toolbars_visibles == true && 'addClass' || 'removeClass')]('frontadmin-show-toolbars')
 
         $self.events = {
 
             // Readjust bar & toolbars on window resize
-            windowResized: function() {
+            onWindowResize: function(e) {
                 $self.toolbars.each(function(){
                     $(this).width($(this).parent().width())
                 })
@@ -27,8 +44,9 @@ $(function(){
                 $self.bar.width(w)
                     .css('margin-left', ww / 2 - (w / 2))
             },
-
-            onLogout: function(){
+            
+            // Log the user out and hide frontadmin
+            onLogout: function(e){
                 $.get($(this).attr('href'), function(){
                     $self.toolbars.each(function(){
                         var wrapper = $(this).parents('.front-admin-block')
@@ -42,26 +60,34 @@ $(function(){
                     })
                 })
                 return false
+            },                
+
+            // toggle frontadmin ui
+            onToggleToolbar: function(e){
+                $('html').toggleClass('frontadmin-show-toolbars')
+                $self.cookie('frontadmin_toolbars_visibles', $('html').hasClass('frontadmin-show-toolbars') && true || false)
+                return false;
             }
         }
 
         $self.bindBarEvents = function() {
             var doc = $self.bar.contents()
+            doc.find($self.buttons.logout).bind('click.frontadmin', $self.events.onLogout).end()
+               .find($self.buttons.toggle).bind('click.frontadmin', $self.events.onToggleToolbar).end()
 
-            // Logout
-            doc.find($self.button.logout).bind('click', $self.events.onLogout)
         }
 
         return {
             init: function() {
-                $self.events.windowResized()
-                $(window).resize($self.windowResized)
+                $self.events.onWindowResize()
+                $(window).resize($self.events.onWindowResize)
+
                 // Little trick to load iframe content locally
                 $self.bar.add($self.toolbars).each(function(){
                     $(this).contents().find('body').html($(this).text())
                 })
                 $self.bindBarEvents()
-                $self.initialized = true
+                $self.states.initialized = true
             }
         }
     })()
