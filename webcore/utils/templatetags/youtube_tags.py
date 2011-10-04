81edd8ee9ded07e04fbf4962722c4872e9a823dd
Maxime Haineault <max@motion-m.ca>
2011-10-04 15:21:03 +0000

    Added template tag to turn a Youtube URL into an embed player

---
+++ webcore/utils/templatetags/youtube_tags.py
@@ -0,0 +1,18 @@
+import os
+
+from django import template
+
+register = template.Library()
+
+@register.simple_tag()
+def embed_player(v, w):
+    """
+    Returns an Youtube embed player for a given video URL
+    """
+    if "embed" not in v:
+        v = v.split('?v=')[1].split('&')[0]
+
+    return '<iframe width="%(w)s" height="%(h)s" src="http://www.youtube.com/embed/%(src)s" frameborder="0" allowfullscreen></iframe>' % {
+                'w': w, 'src': v,
+                'h': int(int(w) * 0.75),
+            }
