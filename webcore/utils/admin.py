a7d43e32e97b9423fe6fb913acb5fce58f0461ba
Maxime Haineault <max@motion-m.ca>
2012-05-09 16:06:10 -0400

    Small fix

--- webcore/utils/admin.py
+++ webcore/utils/admin.py
@@ -4,30 +4,32 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
 from django.db.models.fields.files import ImageFieldFile
 
-from easy_thumbnails.files import get_thumbnailer
-from easy_thumbnails.exceptions import InvalidImageFormatError
+if 'easy_thumbnails' in settings.INSTALLED_APPS:
 
-class AdminThumbnailMixin(object):
-    thumbnail_options = {'size': (60, 60)}
-    thumbnail_image_field_name = 'image'
-    thumbnail_alt_field_name = None
-    thumbnail_404 = ""
+    from easy_thumbnails.files import get_thumbnailer
+    from easy_thumbnails.exceptions import InvalidImageFormatError
 
-    def _thumb(self, image, options={'size': (60, 60)}, alt=None):
-        media = getattr(settings, 'THUMBNAIL_MEDIA_URL', settings.MEDIA_URL)
-        attrs = []
-        try:
-            src = "%s%s" % (media, get_thumbnailer(image).get_thumbnail(options))
-        except InvalidImageFormatError:
-            src = self.thumbnail_404
-        if alt is not None: attrs.append('alt="%s"' % alt)
+    class AdminThumbnailMixin(object):
+        thumbnail_options = {'size': (60, 60)}
+        thumbnail_image_field_name = 'image'
+        thumbnail_alt_field_name = None
+        thumbnail_404 = ""
 
-        return mark_safe('<img src="%s" %s />' % (src, " ".join(attrs)))
+        def _thumb(self, image, options={'size': (60, 60)}, alt=None):
+            media = getattr(settings, 'THUMBNAIL_MEDIA_URL', settings.MEDIA_URL)
+            attrs = []
+            try:
+                src = "%s%s" % (media, get_thumbnailer(image).get_thumbnail(options))
+            except InvalidImageFormatError:
+                src = self.thumbnail_404
+            if alt is not None: attrs.append('alt="%s"' % alt)
 
-    def thumbnail(self, obj):
-        kwargs = {'options': self.thumbnail_options}
-        if self.thumbnail_alt_field_name:
-            kwargs['alt'] = getattr(obj, self.thumbnail_alt_field_name)
-        return self._thumb(getattr(obj, self.thumbnail_image_field_name), **kwargs)
-    thumbnail.allow_tags = True
-    thumbnail.short_description = _('Thumbnail')
+            return mark_safe('<img src="%s" %s />' % (src, " ".join(attrs)))
+
+        def thumbnail(self, obj):
+            kwargs = {'options': self.thumbnail_options}
+            if self.thumbnail_alt_field_name:
+                kwargs['alt'] = getattr(obj, self.thumbnail_alt_field_name)
+            return self._thumb(getattr(obj, self.thumbnail_image_field_name), **kwargs)
+        thumbnail.allow_tags = True
+        thumbnail.short_description = _('Thumbnail')
