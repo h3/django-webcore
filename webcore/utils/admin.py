4fe8db39648e00dfb0ab4b51d37bb7da53173161
h3 <haineault@gmail.com>
2012-12-12 21:50:41 -0500

    Update webcore/utils/admin.py
    Added UserDisplayAdminMixin

--- webcore/utils/admin.py
+++ webcore/utils/admin.py
@@ -4,6 +4,43 @@ from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
 from django.db.models.fields.files import ImageFieldFile
 
+from django.contrib import admin
+from django.contrib.auth.models import User
+
+
+class UserDisplayAdminMixin(admin.ModelAdmin):
+    """
+    In addition to showing a user's username in related fields, show their full
+    name too (if they have one and it differs from the username).
+
+    author: SmileyChris
+    http://djangosnippets.org/snippets/1642/
+    """
+    always_show_username = True
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        field = super(NiceUserModelAdmin, self).formfield_for_foreignkey(
+                                                db_field, request, **kwargs)
+        if db_field.rel.to == User:
+            field.label_from_instance = self.get_user_label
+        return field
+
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        field = super(NiceUserModelAdmin, self).formfield_for_manytomany(
+                                                db_field, request, **kwargs)
+        if db_field.rel.to == User:
+            field.label_from_instance = self.get_user_label
+        return field
+
+    def get_user_label(self, user):
+        name = user.get_full_name()
+        username = user.username
+        if not self.always_show_username:
+            return name or username
+        return (name and name != username and '%s (%s)' % (name, username)
+                or username)
+
+
 if 'easy_thumbnails' in settings.INSTALLED_APPS:
 
     from easy_thumbnails.files import get_thumbnailer
